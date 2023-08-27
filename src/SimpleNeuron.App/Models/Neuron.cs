8a775fd6710b2c10aab68cccb8dd8b09d0ab29aa
Axel Tkacheff <aptkachev@rolftech.ru>
2023-08-27 23:50:32 +0300

    Add Console Application. Add code

---
+++ src/SimpleNeuron.App/Models/Neuron.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleNeuron.App.Models
+{
+    public class Neuron
+    {
+        private decimal _weight = 0.5M;
+        
+        public decimal LastError { get; private set; }
+
+        public decimal Smothing { get; set; } = 0.00001M;
+
+        public decimal ProcessInputData(decimal input)
+        {
+            return _weight * input;
+        }
+
+        public decimal RestoreInputDate(decimal output)
+        {
+            return output / _weight;
+        }
+
+        public void Train(decimal input, decimal expectedResult)
+        {
+            var actualResult = input * _weight;
+
+            LastError = expectedResult - actualResult;
+
+            var correction = (LastError / actualResult) * Smothing;
+
+            _weight += correction;
+        }
+
+
+    }
+}
---
+++ src/SimpleNeuron.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using SimpleNeuron.App.Models;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("press enter for start");
+        Console.ReadLine();
+
+        decimal valueInKilometers = 100;
+        decimal valueInMiles = 62.1371M;
+
+        var neuron = new Neuron();
+
+        var i = 0;
+        do
+        {
+            i++;
+
+            neuron.Train(valueInKilometers, valueInMiles);
+            Console.WriteLine($"Iteration #{i}\tError: \t{neuron.LastError}");
+        } while (neuron.LastError > neuron.Smothing || neuron.LastError < -neuron.Smothing);
+
+        Console.WriteLine($"{neuron.ProcessInputData(valueInKilometers)} miles in {valueInKilometers} km");
+    }
+}
